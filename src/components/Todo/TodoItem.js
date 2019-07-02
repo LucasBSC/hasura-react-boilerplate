384d189365df1d08fef3d8663bdaa7f47ce9d58f
Lucas Bastos <lucas.bastos@interakt.com.br>
2019-07-02 17:13:10 -0300

    Add remove todo

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -5,11 +5,6 @@ import gql from "graphql-tag";
 import { GET_MY_TODOS } from "./TodoPrivateList";
 
 const TodoItem = ({ index, todo, client }) => {
-  const removeTodo = e => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
-
   const TOGGLE_TODO = gql`
     mutation toggleTodo($id: Int!, $isCompleted: Boolean!) {
       update_todos(
@@ -20,6 +15,32 @@ const TodoItem = ({ index, todo, client }) => {
       }
     }
   `;
+
+  const REMOVE_TODO = gql`
+    mutation removeTodo($id: Int!) {
+      delete_todos(where: { id: { _eq: $id } }) {
+        affected_rows
+      }
+    }
+  `;
+
+  const removeTodo = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    client.mutate({
+      mutation: REMOVE_TODO,
+      variables: { id: todo.id },
+      optimisticResponse: {},
+      update: cache => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.filter(t => t.id !== todo.id);
+        cache.writeQuery({
+          query: GET_MY_TODOS,
+          data: { todos: newTodos }
+        });
+      }
+    });
+  };
 
   const toggleTodo = () => {
     client.mutate({
